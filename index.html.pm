a8e58f6889679ba6e3a3970f94183ecca1bcac3b
Christoph Tittel <christoph.tittel@gmail.com>
2021-07-08 22:36:06 +0200

    add about me text

--- index.html.pm
+++ index.html.pm
@@ -1,7 +1,9 @@
 #lang pollen/markup
 ◊define-meta[title]{Index}
 
-◊h1{Welcome to my personal site!}
+◊h2{About Me}
+Hi, my name is Christoph Tittel. I'm currently a Researcher at the Plan-Based Robotics group at DFKI (German Research Center for Artificial Intelligence).
+I received a Master of Science from the Technical University of Munich in ◊i{Robotics, Cognition, Intelligence}, an interdisciplinary program offered by the departments of Informatics, Electrical Engineering and Mechanical Engineering, but focused mostly on computer science side of things. Before that I received a Bachelors from Nuremberg Institute of Technology in Mechatronics. My Bachelor studies were organized in the format of a dual studies program (◊i{Verbundstudium}) in cooperation with Siemens and included a practical training and internships at Siemens.
 
 ◊h2{News}
 No news is good news ;)
