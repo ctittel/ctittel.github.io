5f5f0032e869d567656980e3c81b062923ed4ae3
Christoph Tittel <christoph.tittel@gmail.com>
2021-07-10 22:09:27 +0200

    update about me section

--- index.html.pm
+++ index.html.pm
@@ -2,10 +2,11 @@
 ◊define-meta[title]{Index}
 
 ◊h2{About Me}
-Hi, my name is Christoph Tittel. I'm currently a Researcher at the Plan-Based Robotics group at DFKI (German Research Center for Artificial Intelligence).
-I received a Master of Science from the Technical University of Munich in ◊i{Robotics, Cognition, Intelligence}, an interdisciplinary program offered by the departments of Informatics, Electrical Engineering and Mechanical Engineering. As the name of the program might suggest, it was focused primarily on computer science side of things.
-Before that, I received a Bachelor's in Mechatronics from Nuremberg Institute of Technology. My Bachelor studies had the format of a dual studies program (◊i{Verbundstudium}) in cooperation with Siemens and included a practical training and internships at Siemens.
-I'm originally from a small village in the middle of Franconia, the northern part of Bavaria.
+Hi, my name is Christoph Tittel. I'm currently a Researcher at the ◊hyperlink["https://www.dfki.de/en/web/research/research-departments/plan-based-robot-control/"]{Plan-Based Robot Control group (PBR)} at DFKI (◊i{German Research Center for Artificial Intelligence}).
+I received a Master of Science from the Technical University of Munich in ◊i{Robotics, Cognition, Intelligence}, a joint program offered by the departments of Informatics, Electrical Engineering and Mechanical Engineering.
+As the name of the program might suggest, it was focused primarily on the computer science side of things.
+Before that, I received a Bachelor's in Mechatronics from Nuremberg Institute of Technology. My Bachelor studies were organized in the form of a dual studies program (◊i{Verbundstudium}) in cooperation with Siemens and included a practical training and internships at Siemens.
+I grew up on a farm in a tiny village in the middle of Franconia, the northern part of Bavaria.
 
 ◊h2{News}
 No news is good news ;)
