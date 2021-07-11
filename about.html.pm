b3d5ed35e521456eb206d5fa568eb57cec116e68
Christoph Tittel <christoph.tittel@gmail.com>
2021-07-11 16:21:01 +0200

    add About page

---
+++ about.html.pm
@@ -0,0 +1,21 @@
+#lang pollen/markup
+◊define-meta[title]{About}
+
+◊h2{About This Site}
+
+◊p{I have always been fascinated by the idea of having a place in the internet where I can present myself and write about my thoughts and interests.
+When the end of my masters studies approached I thought that now is the time for this.
+For the domain name, I wanted a domain with only my surename (◊code{tittel}), but unfortunately the common TLDs (◊code{.com}, ◊code{.net} etc.) were already taken (which taught me that my surename is more common than I thought).
+I also checked out some of the new gTLDs but ultimatively decided against using one of these when I learned that less-computer savy people might not recognize a domain like ◊code{<something>.xyz} as a web address. 
+Also, I read that some e-mail providers and corporations might flag e-mails coming from addresses with gTLDs which are often used for sketchy purposes as spam.}
+
+◊p{Next step was deciding for an architecture for the site.
+Static site generation is good enough for my purpose because it is simple to develop, cheap to host and has a long lifespan which doesn't require major changes or updates if the stack is simple and stable enough. Additionally, static, pregenerated sites can usually be served very quickly if they don't contain too much stuff, thus also providing a very responsive and smooth user experience for free.}
+
+◊p{My greatest priorities for the site are:
+◊ul{
+    ◊li{Simplicity of technology: The tech used for the site must be so simple that I am able to understand every single little detail about it (the CSS sheets, any JavaScript, the HTML, etc.)}
+    ◊li{Customizability: If I get new ideas for the site, I want to be able to implement them with as little difficulty as possible.}
+    ◊li{Longevity: I want the (build-)requirements for the website to be as basic and simple as possible so that the site will likely never require a major "rewrite" to a different technology stack.}
+    ◊li{Simplicity of design: The design must be simple, timeless and straightforward so users never get confused.}
+}}
