4e8847c16bbcababc0f0139d369cc4af0dc1f1f8
Christoph Tittel <christoph.tittel@gmail.com>
2021-07-11 22:00:34 +0200

    add section about my interests

--- index.html.pm
+++ index.html.pm
@@ -10,8 +10,13 @@ Before that, I received a Bachelor's in Mechatronics from Nuremberg Institute of
 I grew up on a farm in a tiny village in the middle of ◊hyperlink["https://en.wikipedia.org/wiki/Franconia"]{Franconia}, the northern part of Bavaria.
 }
 
-◊h2{News}
-No news is good news ;)
+◊h2{Interests}
+This is a list of some of my current interests (there are more, of course). I do not claim to be an expert in anything. But I find these fields interesting and think that they are important:
+◊ul{
+    ◊li{◊b{Artificial Intelligence}: Currently, I prefer AI approaches which make no "explicit" use of statistics. My current belief is that the most promising approach towards ◊hyperlink["https://en.wikipedia.org/wiki/Artificial_general_intelligence"]{AGI} is the combination of a Machine Learning approach like Deep Learning plus some sort of genetic approach like ◊hyperlink["https://geneticprogramming.com"]{Genetic Programming}.}
+    ◊li{◊b{◊hyperlink["https://en.wikipedia.org/wiki/Information_design"]{Information Design}}: Information Design wants to answer the question of how we can present information (mostly visual information like text and images) so that it can be absorbed as easily as possible by other humans, taking human psychology and cognition into account. One of the key scientists in this field is Edward Tufte, who describes many ◊hyperlink["https://www.edwardtufte.com/tufte/"]{interesting insights} about this field. A nice example for Information Design, and an easy way to use the results of the field in a website, is ◊hyperlink["https://edwardtufte.github.io/tufte-css/"]{tufte-css}.}
+    ◊li{◊b{Programming Languages}: I enjoy learning about new programming languages and how they compare to existing ones. Also, I am curious towards which programming languages humanity will "converge" in the long term. An apparently often underestimated factor in newer programming languages is their UX and the ◊hyperlink["https://en.wikipedia.org/wiki/Psychology_of_programming"]{Psychology of Programming}.}
+}
 
 ◊h2{Links}
 
