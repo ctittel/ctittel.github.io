5bf15cc58026a8c4e65f34c34c6d07b69badf000
Christoph Tittel <c@ctittel.com>
2022-01-06 01:50:43 +0100

    Update Index (small changes)

--- site/index.html.pm
+++ site/index.html.pm
@@ -6,7 +6,7 @@
 Hi, my name is Christoph Tittel. I'm currently a Researcher at the ◊hyperlink["https://www.dfki.de/en/web/research/research-departments/plan-based-robot-control/"]{Plan-Based Robot Control group (PBR)} at DFKI (◊i{German Research Center for Artificial Intelligence}).
 I received a Master of Science from the Technical University of Munich in ◊hyperlink["https://www.tum.de/en/studies/degree-programs/detail/robotics-cognition-intelligence-master-of-science-msc/"]{Robotics, Cognition, Intelligence}, a joint program offered by the departments of Informatics, Electrical Engineering and Mechanical Engineering.
 As the name of the program might suggest, it was focused primarily on the computer science side of things.
-Before that, I received a Bachelor's in Mechatronics from Nuremberg Institute of Technology. My Bachelor studies were organized in the form of a dual studies program (◊i{Verbundstudium}) in cooperation with Siemens and included a practical training and internships at Siemens.
+Before that, I received a Bachelor's in Mechatronics from the Technische Hochschule Nürnberg. My Bachelor studies were organized in the form of a dual studies program (◊i{Verbundstudium}) in cooperation with Siemens and included a practical training and internships in cooperation with the conglomerate Siemens.
 I grew up on a farm in a tiny village in the middle of ◊hyperlink["https://en.wikipedia.org/wiki/Franconia"]{Franconia}, the northern part of Bavaria.
 }
 
